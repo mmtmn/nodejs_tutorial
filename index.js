ce92cc735f7ba188aff34e37f1e4baff14f3e56f
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-09-18 14:27:01 -0300

    Add express, index.html, sobre.html

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const http = require('http');
+const express = require('express');
+const app = express	();
+const path = require('path');
+const router = express.Router();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+router.get('/',function(req,res){
 
+    res.sendFile(path.join(__dirname+'/index.html'));
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/pain');
-    res.end('Hello world from nodejs')
-});
-
-server.listen(port,hostname, () => {
-    console.log("servidor rodando");
 })
+
+router.get('/sobre',function(req,res){
+
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+
+})
+
+app.use('/',router);
+app.listen(process.env.port || 3000);
+
+console.log("Servidor local rodando")
